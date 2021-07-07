7b667b09c8731408787d902877e45d58b61ee3e3
Dhananjay <dnanthriss@gmail.com>
2021-07-08 01:11:49 +0530

    covid tracking

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBumNB-QVZhQFMYjUSvcpihtoeSSrDPy-c",
+    authDomain: "covid-statistics-c5631.firebaseapp.com",
+    projectId: "covid-statistics-c5631",
+    storageBucket: "covid-statistics-c5631.appspot.com",
+    messagingSenderId: "883768527665",
+    appId: "1:883768527665:web:a0c2971cfb949ee4b1fe10",
+    measurementId: "G-PE2V02XY7T"
+  };
